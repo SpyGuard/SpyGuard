b0f6067572963aee98203935397e6a1b74206006
b068931cc450442b63f5b3d276ea4297 <77171083+b068931cc450442b63f5b3d276ea4297@users.noreply.github.com>
2024-01-08 17:42:29 +0100

    rename spy to SpyGuard

--- install.sh
+++ install.sh
@@ -91,7 +91,7 @@ Description=Spyguard frontend service
 
 [Service]
 Type=simple
-ExecStart=$USER_HOME/spy-venv/bin/python3 /usr/share/spyguard/server/frontend/main.py
+ExecStart=$USER_HOME/SpyGuard-venv/bin/python3 /usr/share/spyguard/server/frontend/main.py
 Restart=on-abort
 KillMode=process
 
@@ -106,7 +106,7 @@ Description=Spyguard backend service
 
 [Service]
 Type=simple
-ExecStart=$USER_HOME/spy-venv/bin/python3 /usr/share/spyguard/server/backend/main.py
+ExecStart=$USER_HOME/SpyGuard-venv/bin/python3 /usr/share/spyguard/server/backend/main.py
 Restart=on-abort
 KillMode=process
 
@@ -123,7 +123,7 @@ After=network-online.target
 
 [Service]
 Type=simple
-ExecStart=$USER_HOME/spy-venv/bin/python3 /usr/share/spyguard/server/backend/watchers.py
+ExecStart=$USER_HOME/SpyGuard-venv/bin/python3 /usr/share/spyguard/server/backend/watchers.py
 Restart=on-abort
 KillMode=process
 
@@ -186,7 +186,7 @@ check_dependencies() {
        fi
    done
    echo -e "\e[39m[+] Install Python packages...\e[39m"
-   python3 -m venv $USER_HOME/spy-venv && source $USER_HOME/spy-venv/bin/activate && python3 -m pip install -r "$SCRIPT_PATH/assets/requirements.txt"
+   python3 -m venv $USER_HOME/SpyGuard-venv && source $USER_HOME/SpyGuard-venv/bin/activate && python3 -m pip install -r "$SCRIPT_PATH/assets/requirements.txt"
 }
 
 get_version() {
